deef2b8e3ff1788d8675c21275ef297f87698a22
Sergio Urbina <surbina.db@gmail.com>
2026-07-15 11:51:55 -0600

    El Dispensador de Píldoras // The Pill Dispenser

---
+++ JAVA-Set/p152.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class p152 {
+    public static void main(String[] args) {
+        pillDispenser Object=new pillDispenser();
+    }
+}
+class pillDispenser{
+    int a_cases;
+    Scanner a_keyboard = new Scanner(System.in);
+    public void m_getCases(){a_cases=a_keyboard.nextInt();}
+
+}
